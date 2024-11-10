5ae3334af2c089605956f88915e9ed850ff43d9d
a.nikitin52 <a.nikitin0552@gmail.com>
2024-11-10 19:32:46 +0300

    class list and iterator added

--- include/tlist.h
+++ include/tlist.h
@@ -8,6 +8,8 @@
 
 using namespace std;
 
+
+
 template<typename T>
 class TList
 {
@@ -15,95 +17,186 @@ class TList
 	{
 		T data;
 		Node* next;
+        Node(T data, Node* next) : data(data), next(next) {}
 	};
 	Node* first;
 
 public:
-	TList() {
-		first(nullptr);
-	}
-
-	TList(int n) {
-		if (n == 0) return;
-		first = new Node(T(), nullptr);
-		Node* current = first;
-		for (int i = 0; i < n; i++) {
-			Node* tmp = new Node(T(); nullptr);
-			current->next = tmp;
-			current = current->next;
-		}
-	}
-
-	TList(const List& l) {
-		if (!l.first) {
-
-		}
-		this->first = new Node(other.first->data, nullptr);
-		Node* current = first, ocurrent{};
-		while (ocurrent->next)
-		{
-			current->next = new Node(ocurrent->next->data, nullptr);
-		}
-		current = current->next;
-		ocurrent = ocurrent->next;
-	}
-
-	~TList() {
-		while (first) {
-			Node* second = first->next;
-			delete first;
-			first = second;
-		}
-	}
-
-	TList(int n, T dflt = T());
 	
-	TList(const List& l);
+    TList() {
+        first = nullptr;
+    }
 
-	TList& operator=(const tList& l);
-	T& operator[] (int index);
+    TList(int n) {
+        if (n < 0) {
+            throw "Error!";
+        }
+        if (n == 0) {
+            first = nullptr;
+            return;
+        }
+        first = new Node(T(), nullptr);
+        Node* current = first;
+        for (int i = 1; i < n; i++) {
+            Node* tmp = new Node(T(), nullptr);
+            current->next = tmp;
+            current = current->next;
+        }
+    }
 
-	Node* insert(T value, Node* prev) {
-		Node* tmp = new Node;
-		tmp->next = prev->next;
-		tmp->data = value;
-		prev->next = tmp;
-		return tmp;
-	}
+    TList(const TList& other) {
+        if (!other.first) {
+            first = nullptr;
+            return;
+        }
+        first = new Node(other.first->data, nullptr);
+        Node* current = first;
+        Node* ocurrent = other.first->next;
+        while (ocurrent) {
+            current->next = new Node(ocurrent->data, nullptr);
+            current = current->next;
+            ocurrent = ocurrent->next;
+        }
+    }
 
-	Node* insert_front(T value) {
-		Node* first_first = new Node;
-		first_first->next = first;
-		first_fisrst->data = value;
-		first = first_first;
-		return first;
-	}
+    ~TList() {
+        while (first) {
+            Node* second = first->next;
+            delete first;
+            first = second;
+        }
+    }
 
-	Node* erase(Node* prev) {
-		Node* tmp = prev->next;
-		if (!prev->next) throw 1;
-		prev->next = tmp->next;
-		delete tmp;
-		return prev_next;
-	}
+    TList(int n, T dflt) {
+        if (n < 0) {
+            throw "Error!";
+        }
+        if (n == 0) {
+            first = nullptr;
+            return;
+        }
+        first = new Node(dflt, nullptr);
+        Node* current = first;
+        for (int i = 1; i < n; i++) {
+            Node* tmp = new Node(dflt, nullptr);
+            current->next = tmp;
+            current = current->next;
+        }
+    }
 
-	Node* erase_front() {
-		Node* tmp = first;
-		first = tmp->next;
-		delete tmp;
-		return first;
-	}
+    TList& operator=(const TList& l) {
+        if (this == &l) {
+            return *this;
+        }
+        while (first) {
+            Node* second = first->next;
+            delete first;
+            first = second;
+        }
+        if (!l.first) {
+            first = nullptr;
+            return *this;
+        }
+        first = new Node(l.first->data, nullptr);
+        Node* current = first;
+        Node* ocurrent = l.first->next;
+        while (ocurrent) {
+            current->next = new Node(ocurrent->data, nullptr);
+            current = current->next;
+            ocurrent = ocurrent->next;
+        }
+        return *this;
+    }
+	
 
-	Node* find(T value);
+    T& operator[] (int index) {
+        if (index < 0) {
+            throw "Error!";
+        }
+        Node* current = first;
+        for (int i = 0; i < index; i++) {
+            if (current == nullptr) {
+                throw "Error!";
+            }
+            current = current->next;
+        }
+        if (current == nullptr) {
+            throw "Error!";
+        }
+        return current->data;
+    }
 
-	size_t size() {
-		int size = 0;
-		Node* current = first;
-		while (current) {
-			size += 1;
-			current = current->next;
-		}
-	}
+    Node* insert(T value, Node* prev) {
+        if (prev == nullptr) {
+            throw "Error!";
+        }
+        Node* tmp = new Node(value, prev->next);
+        prev->next = tmp;
+        return tmp;
+    }
+    
+    Node* erase(Node* prev) {
+        if (prev == nullptr) {
+            throw "Error!";
+        }
+        Node* tmp = prev->next;
+        if (!prev->next) throw "Error!";
+        prev->next = tmp->next;
+        delete tmp;
+        return prev->next;
+        
+    }
+    
+    Node* find(T value) {
+        Node* current = first;
+        while (current) {
+            if (current->data == value) {
+                return current;
+            }
+            current = current->next;
+        }
+        return nullptr;
+    }
+    
+    size_t size() {
+        int size = 0;
+        Node* current = first;
+        while (current) {
+            size += 1;
+            current = current->next;
+        }
+        return size;
+    }
+
+    void print();
+	
+    class iterator {
+        Node* cur;
+        explicit iterator(Node* node) : cur(node) {}
+    public:
+        iterator& operator++() {
+            cur = cur->next;
+            return *this;
+        }
+        iterator operator++(int) {
+            iterator copy = *this;
+            cur = cur->next;
+            return copy;
+        }
+        T* operator->() {
+            return &(cur->data);
+        }
+        friend bool operator!=(const iterator it1, const iterator it2) {
+            return it1.cur != it2.cur;
+        }
+    };
+
+    iterator begin() {
+        return iterator(first);
+    }
+    iterator end() {
+        return iterator(nullptr);
+    }
 };
 
 
--- test/test_list.cpp
+++ test/test_list.cpp
@@ -2,7 +2,75 @@
 
 #include <gtest.h>
 
-TEST(TList, can_create_list_with_positive_length) {
-	ASSERT_NO_THROW(TList<int> l(3));
+TEST(TList, can_create_list) {
+	ASSERT_NO_THROW(TList<int> l);
 }
+TEST(TList, can_create_list_with_positive_length) {
+	ASSERT_NO_THROW(TList<int> l(5));
+}
+TEST(TList, cant_create_list_with_negative_length) {
+	ASSERT_ANY_THROW(TList<int> l(-2));
+}
+TEST(TList, create_list_with_zero_length) {
+	ASSERT_NO_THROW(TList<int> l(0));
+}
+TEST(TList, copy_list) {
+	const int size = 5;
+	TList<int> l1(size);
+
+	ASSERT_NO_THROW(TList<int> l2(l1));
+}
+TEST(TList, copy_list_itself) {
+	const int size = 5;
+	TList<int> l1(size);
+
+	ASSERT_NO_THROW(TList<int> l1(l1));
+}
+TEST(TList, copy_list_has_equal_size) {
+	const int size = 5;
+	TList<int> l1(size);
+	TList<int> l2(l1);
+	ASSERT_EQ(size, l2.size());
+}
+TEST(TList, can_get_element_with_positive_index) {
+	const int size = 3;
+	TList<int> l1(size);
+	for (int i = 0; i < size; i++) {
+		l1[i] = i;
+	}
+	ASSERT_EQ(l1[2], 2);
+}
+TEST(TList, cant_get_element_with_negative_index) {
+	const int size = 3;
+	TList<int> l1(size);
+	ASSERT_ANY_THROW(l1[-1]);
+}
+TEST(TList, cant_get_element_with_index_more_than_list_size) {
+	const int size = 3;
+	TList<int> l1(size);
+	ASSERT_ANY_THROW(l1[15]);
+}
+TEST(TList, can_insert_element) {
+	TList<int> lst(5, 1);
+	lst.insert(10, lst.find(1));
+	ASSERT_EQ(lst[1], 10);
+}
+TEST(TList, can_erase_element) {
+	TList<int> lst(5, 1);
+	lst.erase(lst.find(1));
+	ASSERT_NO_THROW(lst.erase(lst.find(1)));
+}
+TEST(TList, can_find_elenent_in_list) {
+	TList<int> lst(5, 1);
+	ASSERT_NE(lst.find(1), nullptr);
+}
+TEST(TList, cant_find_elenent_in_list) {
+	TList<int> lst(5, 1);
+	ASSERT_EQ(lst.find(10), nullptr);
+}
+TEST(TList, can_get_size) {
+	TList<int> lst(5, 1);
+	ASSERT_EQ(lst.size(), 5);
+}
+
 
